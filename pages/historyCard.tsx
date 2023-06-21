38d6d68bebb0397d15641a8b975409fa07b9c398
arkteck <arkteck@gmail.com>
2023-06-21 12:08:34 -0400

    add Japanese name and duration

--- pages/historyCard.tsx
+++ pages/historyCard.tsx
@@ -8,20 +8,27 @@ import { HistoryProperties, VillagerProperties } from '../types';
 export default function HistoryCard({history, villagerData}: {history: HistoryProperties, villagerData: VillagerProperties | undefined}) {
   return (
     <Paper elevation={2}>
-      <Box padding={1}>
+      <Box padding={1} bgcolor={history.currentResident ? 'success.light' : ''}>
         <Stack>
           <Avatar
             variant="square"
             src={`http://acnhapi.com/v1/images/villagers/${villagerData?.id}`}
+            sx={{ width: 150, height: 150 }}
           />
           <Typography>
             {history?.name}
+          </Typography>
+          <Typography>
+            {villagerData?.name['name-JPja']}
           </Typography>
           <Typography>
             {history?.startDate.toLocaleDateString("en-ZA")}
           </Typography>
           <Typography>
             {!history?.currentResident ? history?.endDate.toLocaleDateString("en-ZA") : 'Present'}
+          </Typography>
+          <Typography>
+            {Math.round((history?.endDate.getTime() - history?.startDate.getTime()) / (1000 * 3600 * 24)) + 1} days
           </Typography>
         </Stack>
       </Box>
