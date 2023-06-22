0155d4eaf4c2b5eef4525ea7bf9e37f4ed5495e7
arkteck <arkteck@gmail.com>
2023-06-22 10:44:34 -0400

    refactor to use MUI Card component

--- pages/historyCard.tsx
+++ pages/historyCard.tsx
@@ -1,34 +1,40 @@
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Paper from '@mui/material/Paper';
+import { useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CardActionArea from '@mui/material/CardActionArea';
+import Collapse from '@mui/material/Collapse';
 import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
 import { HistoryProperties, VillagerProperties2 } from '../types';
 
 export default function HistoryCard({history, villagerData}: {history: HistoryProperties, villagerData: VillagerProperties2 | undefined}) {
+
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Paper elevation={2}>
-      <Box padding={1} bgcolor={history?.currentResident ? 'success.light' : ''}>
-        <Stack>
-          <Avatar
-            variant="square"
-            src={villagerData?.nh_details.photo_url}
-            sx={{ width: 150, height: 150 }}
-          />
-          <Typography>
-            {history?.name}
-          </Typography>
-          <Typography>
-            {history?.startDate.toLocaleDateString("en-ZA")}
-          </Typography>
-          <Typography>
-            {!history?.currentResident ? history?.endDate.toLocaleDateString("en-ZA") : 'Present'}
-          </Typography>
-          <Typography>
-            {Math.round((history?.endDate.getTime() - history?.startDate.getTime()) / (1000 * 3600 * 24)) + 1} days
-          </Typography>
-        </Stack>
-      </Box>
-    </Paper>
+    <Card variant="outlined" onClick={() => setExpanded((a)=>!a)}>
+      <CardActionArea>
+        <CardMedia
+          image={villagerData?.nh_details.photo_url}
+          sx={{ width: 150, height: 150 }}
+        />
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography>
+              {history?.name}
+            </Typography>
+            <Typography>
+              {history?.startDate.toLocaleDateString("en-ZA")}
+            </Typography>
+            <Typography>
+              {!history?.currentResident ? history?.endDate.toLocaleDateString("en-ZA") : 'Present'}
+            </Typography>
+            <Typography>
+              {Math.round((history?.endDate.getTime() - history?.startDate.getTime()) / (1000 * 3600 * 24)) + 1} days
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </CardActionArea>
+    </Card>
   );
 }
