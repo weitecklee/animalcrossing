451358b10d8989a1e3ed1bbac73fbe8138779d28
arkteck <arkteck@gmail.com>
2023-06-22 14:21:20 -0400

    add Leaf icon to topBar

--- components/topBar.tsx
+++ components/topBar.tsx
@@ -3,13 +3,15 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
 
 export default function TopBar() {
   return (
     <>
       <AppBar>
         <Toolbar>
-          <Stack direction="row" spacing={1}>
+          <Stack direction="row" spacing={1} alignItems="center">
+            <Avatar variant="square" src="/Animal_Crossing_Leaf.svg" />
             <Typography variant="body1" fontWeight="700">
               My Animal Crossing Island
             </Typography>
