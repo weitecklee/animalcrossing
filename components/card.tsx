d0ecf614fe9e6c9cd77c642efe56cf12f1409e7c
arkteck <arkteck@gmail.com>
2023-06-27 16:05:09 -0400

    Add some sizing changes for small screens in TopBar and Card, plus responsiveFontSizes

--- components/card.tsx
+++ components/card.tsx
@@ -3,52 +3,84 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import CardActionArea from '@mui/material/CardActionArea';
-import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
 import InfoIcon from '@mui/icons-material/Info';
 import { HistoryProperties, VillagerProperties2 } from '../types';
 
 export default function HistoryCard({history, villagerData, setShowDialog, setDialogVillager}: {history: HistoryProperties, villagerData: VillagerProperties2, setShowDialog: Dispatch<SetStateAction<boolean>>, setDialogVillager: Dispatch<SetStateAction<string>>}) {
 
   const [expanded, setExpanded] = useState(false);
+  const Content = ({mobile = false}) => <>
+    <Typography variant={mobile ? 'subtitle2' : 'h6'}>
+      {history.name}
+    </Typography>
+    <Typography variant={mobile ? 'caption' : 'body1'}>
+      {history.startDateString}
+    </Typography>
+    <Typography variant={mobile ? 'caption' : 'body1'}>
+      {!history.currentResident ? history.endDateString : 'Present'}
+    </Typography>
+    <Typography variant={mobile ? 'caption' : 'body1'}>
+      {history.duration} days
+    </Typography>
+    <IconButton
+      onClick={() => {
+        setDialogVillager(history.name);
+        setShowDialog(true);
+      }}
+      size={mobile ? "small" : "medium"}
+      sx={{
+        position: 'absolute',
+        bottom: mobile ? '15%' : 0,
+        right: mobile ? '15%' : 0,
+      }}
+    >
+      <InfoIcon fontSize="inherit" />
+    </IconButton>
+  </>
 
   return (
-    <Card variant="outlined">
+    <Card variant="outlined" sx={{position: 'relative'}}>
       <CardActionArea onClick={() => setExpanded((a)=>!a)}>
         <CardMedia
           image={villagerData.nh_details.photo_url}
-          sx={{ width: 150, height: 150 }}
-        />
+          sx={{ width: {
+            xs: 100,
+            md: 150,
+          }, height: {
+            xs: 100,
+            md: 150,
+          } }}
+        >
+        </CardMedia>
       </CardActionArea>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
+      <Stack
+        height={100}
+        width={100}
+        bgcolor='rgba(255, 255, 255, .8)'
+        justifyContent="flex-start"
+        alignItems="flex-start"
+        padding={1}
+        sx={{
+          display: expanded ? {xs: 'flex', md: 'none'} : 'none',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+        }}
+      >
+        <Content mobile={true} />
+      </Stack>
+      <Collapse in={expanded} timeout="auto" unmountOnExit sx={{
+        display: {
+          xs: 'none',
+          md: 'flex',
+        }
+      }}>
         <CardContent sx={{ position: 'relative' }}>
-          <Typography variant="h6">
-            {history.name}
-          </Typography>
-          <Typography>
-            {history.startDateString}
-          </Typography>
-          <Typography>
-            {!history.currentResident ? history.endDateString : 'Present'}
-          </Typography>
-          <Typography>
-            {history.duration} days
-          </Typography>
-          <IconButton
-            onClick={() => {
-              setDialogVillager(history.name);
-              setShowDialog(true);
-            }}
-            sx={{
-              position: 'absolute',
-              bottom: 0,
-              right: 0,
-            }}
-          >
-            <InfoIcon />
-          </IconButton>
+          <Content />
         </CardContent>
       </Collapse>
     </Card>
--- components/topBar.tsx
+++ components/topBar.tsx
@@ -1,18 +1,53 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useState, MouseEvent } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Image from 'next/image';
-import LeafIcon from '../public/Animal_Crossing_Leaf.svg'
+import LeafIcon from '../public/Animal_Crossing_Leaf.svg';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
 
 const pages = ['Villagers', 'Timeline'];
 
 export default function TopBar({setComponent}: { setComponent: Dispatch<SetStateAction<string>> }) {
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
       <AppBar color="secondary">
         <Toolbar>
+          <Box sx={{display: {xs: 'flex', md: 'none'}}}>
+            <IconButton onClick={handleClick}>
+              <MenuIcon/>
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+            >
+              {pages.map((page) => <MenuItem
+                key={page}
+                onClick={() => {
+                  setComponent(page);
+                  handleClose();
+                }}
+              >
+                {page}
+              </MenuItem>)}
+            </Menu>
+          </Box>
           <Button
             onClick={() => setComponent('Index')}
           >
@@ -25,15 +60,17 @@ export default function TopBar({setComponent}: { setComponent: Dispatch<SetState
           <Typography variant="h5" fontWeight="500" sx={{flexGrow: 1}}>
             My Animal Crossing Island
           </Typography>
-          {pages.map((page) => (
-            <Button
-              key={page}
-              sx={{color:'black', mx: 1}}
-              onClick={() => setComponent(page)}
-            >
-              {page}
-            </Button>
-          ))}
+          <Box sx={{display: {xs: 'none', md: 'flex'}}}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                sx={{color:'black', mx: 1}}
+                onClick={() => setComponent(page)}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
         </Toolbar>
       </AppBar>
       <Toolbar />
