7af638aab68ac81699e38c372ffe1e76775bfc30
arkteck <arkteck@gmail.com>
2023-06-29 12:24:13 -0400

    changed how duration stats are handled to accommodate ties

--- components/stats.tsx
+++ components/stats.tsx
@@ -5,12 +5,12 @@ import Image from 'next/image';
 import { Link, Dialog, DialogContent, Box, List, ListItem } from '@mui/material';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
-import { VillagerProperties2, HistoryProperties, TraitProperties } from '../types';
+import { VillagerProperties2, HistoryProperties, TraitProperties, DurationProperties } from '../types';
 
-export default function Stats({ villagersData, histories, sortedDurations, speciesData, personalityData, genderData } : {
+export default function Stats({ villagersData, histories, durationData, speciesData, personalityData, genderData } : {
   villagersData: Map<string,VillagerProperties2>,
   histories: Map<string,HistoryProperties>,
-  sortedDurations: string[],
+  durationData: DurationProperties[],
   speciesData: TraitProperties[],
   personalityData: TraitProperties[],
   genderData: TraitProperties[],
@@ -20,7 +20,7 @@ export default function Stats({ villagersData, histories, sortedDurations, speci
   const [showDialog, setShowDialog] = useState(false);
   const [showDurationDialog, setShowDurationDialog] = useState(false);
   const theme = useTheme();
-  const smallScreen = useMediaQuery(theme.breakpoints.down('md'))
+  const smallScreen = useMediaQuery(theme.breakpoints.down('md'));
 
   const VillagerIcon = ({villager}: {villager: string}) => (
     <Image
@@ -30,7 +30,7 @@ export default function Stats({ villagersData, histories, sortedDurations, speci
       width={smallScreen ? 40 : 64}
       title={villager}
     />
-  )
+  );
 
   const IconGrid = ({traitData}: {traitData: TraitProperties}) => (
     <Grid container>
@@ -40,7 +40,7 @@ export default function Stats({ villagersData, histories, sortedDurations, speci
         </Grid>
       )}
     </Grid>
-  )
+  );
 
   const TraitDialog = () => (
     <Dialog
@@ -57,7 +57,7 @@ export default function Stats({ villagersData, histories, sortedDurations, speci
         </>))}
       </DialogContent>
     </Dialog>
-  )
+  );
 
   const BreakdownLink = ({traitData} : {traitData: TraitProperties[]}) => (
     <Link
@@ -70,7 +70,7 @@ export default function Stats({ villagersData, histories, sortedDurations, speci
     >
       Full breakdown
     </Link>
-  )
+  );
 
   return <>
     <Typography>
@@ -78,13 +78,13 @@ export default function Stats({ villagersData, histories, sortedDurations, speci
       <br />
       Average duration of residence: {(Array.from(histories.values()).reduce((a, b) => a + b.duration, 0) / histories.size).toFixed(2)} days
       <br />
-      Longest duration of residence: {histories.get(sortedDurations[0])?.duration} days
+      Longest duration of residence: {durationData[0].duration} days
       <br />
-      <VillagerIcon villager={sortedDurations[0]} />
+      {durationData[0].villagers.map((villager) => <VillagerIcon key={villager} villager={villager} />)}
       <br />
-      Shortest duration of residence: {histories.get(sortedDurations[sortedDurations.length - 1])?.duration} days
+      Shortest duration of residence: {durationData[durationData.length - 1].duration} days
       <br />
-      <VillagerIcon villager={sortedDurations[sortedDurations.length - 1]} />
+      {durationData[durationData.length - 1].villagers.map((villager) => <VillagerIcon key={villager} villager={villager} />)}
       <br />
       <Link
         href="#"
@@ -118,14 +118,17 @@ export default function Stats({ villagersData, histories, sortedDurations, speci
           * = Current Resident
         </Typography>
         <List>
-          {sortedDurations.map((villager) => <ListItem key={villager} disablePadding>
-            <Box display="flex" alignItems="center">
-              <VillagerIcon villager={villager} />
-              <Typography>
-                &nbsp;&nbsp;{histories.get(villager)?.duration} days{histories.get(villager)?.currentResident ? "*" : ""}
-              </Typography>
-            </Box>
-          </ListItem>)}
+          {durationData.map((duration) => (
+            duration.villagers.map((villager) => (
+              <ListItem key={villager} disablePadding>
+                <Box display="flex" alignItems="center">
+                  <VillagerIcon villager={villager} />
+                  <Typography>
+                    &nbsp;&nbsp;{histories.get(villager)?.duration} days{histories.get(villager)?.currentResident ? "*" : ""}
+                  </Typography>
+                </Box>
+              </ListItem>
+          ))))}
         </List>
       </DialogContent>
     </Dialog>
