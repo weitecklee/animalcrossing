670b02fd25a44f28f19ce32a73974a7ee63c0788
arkteck <arkteck@gmail.com>
2023-07-12 11:30:22 -0400

    still ironing out bugs

--- components/customDialog.tsx
+++ components/customDialog.tsx
@@ -18,16 +18,16 @@ export default function CustomDialog({setOpen, hash, ...props} : CustomDialogPro
     };
     window.addEventListener("hashchange", onHashChange);
     return () => window.removeEventListener("hashchange", onHashChange);
-  }, [hash, onClose, open, setOpen]);
+  }, [hash, onClose, open]);
 
   useEffect(() => {
     if (open) {
       window.location.hash = hash;
     } else if (window.location.hash === hash || window.location.hash === "#villagerDialog") {
-      setOpen(false);
+      setOpen2(false);
       window.history.back();
     }
-  }, [hash, open, setOpen]);
+  }, [hash, open]);
 
   return <Dialog
     {...props}
