c168f247bda59d0c77d3aff1bca01da1867a2452
arkteck <arkteck@gmail.com>
2023-07-25 13:55:49 -0400

    Revert "Merge pull request #36 from weitecklee/miscEdits"
    This reverts commit 9a68757d61d7c68a9bc192010b46391fd52b5a23, reversing
    changes made to 324e138cd58f24c35210bd5c53685ba5561c627d.

--- components/card.tsx
+++ components/card.tsx
@@ -1,5 +1,6 @@
+import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-import { Card, CardMedia, IconButton, Stack, Typography, useMediaQuery } from '@mui/material';
+import { Card, CardActionArea, CardMedia, IconButton, Stack, Typography, useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { HistoryProperties, VillagerProperties2 } from '../types';
@@ -13,7 +14,6 @@ export default function HistoryCard({ history, villagerData, setShowVillagerDial
   ) {
 
   const [expanded, setExpanded] = useState(false);
-  const [hover, setHover] = useState(false);
   const theme = useTheme();
   const mediumScreen = useMediaQuery(theme.breakpoints.down('md'))
 
@@ -46,26 +46,36 @@ export default function HistoryCard({ history, villagerData, setShowVillagerDial
     >
       <InfoOutlinedIcon fontSize="inherit" />
     </IconButton>
+    <IconButton
+      onClick={() => {
+        setExpanded(false);
+      }}
+      size={mediumScreen ? "small" : "medium"}
+      sx={{
+        position: 'absolute',
+        top:   '0px' ,
+        right:  '15px',
+      }}
+    >
+      <HighlightOffRoundedIcon fontSize="inherit" />
+    </IconButton>
   </>
 
   return (
-    <Card
-      variant="outlined" sx={{position: 'relative'}}
-      onClick={() => setExpanded((a) => !a)}
-      onMouseOver={() => {setHover(true);}}
-      onMouseOut={() => {setHover(false);}}
-    >
-      <CardMedia
-        image={villagerData.nh_details.photo_url}
-        sx={{ width: {
-          xs: 100,
-          md: 150,
-        }, height: {
-          xs: 100,
-          md: 150,
-        } }}
-      >
-      </CardMedia>
+    <Card variant="outlined" sx={{position: 'relative'}}>
+      <CardActionArea onClick={() => setExpanded((a)=>!a)}>
+        <CardMedia
+          image={villagerData.nh_details.photo_url}
+          sx={{ width: {
+            xs: 100,
+            md: 150,
+          }, height: {
+            xs: 100,
+            md: 150,
+          } }}
+        >
+        </CardMedia>
+      </CardActionArea>
       <Stack
         height="100%"
         width="100%"
@@ -74,7 +84,7 @@ export default function HistoryCard({ history, villagerData, setShowVillagerDial
         alignItems="flex-start"
         padding={1}
         sx={{
-          display: expanded || hover ? 'flex' : 'none',
+          display: expanded ? 'flex' : 'none',
           position: 'absolute',
           top: 0,
           left: 0,
