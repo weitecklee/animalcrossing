c3acd942fd4526d5f78c06b71cb46ea2da73b5ee
arkteck <arkteck@gmail.com>
2023-07-27 10:59:10 -0400

    refactor to put code in separate prepareData

---
+++ lib/prepareData.ts
@@ -0,0 +1,151 @@
+import { villagersData } from '../lib/combinedData';
+import { calculateDays } from '../lib/functions';
+import { DurationProperties, HistoryProperties, MongoProperties, PhotoStats2Properties } from '../types';
+
+export default function prepareData(mongoData: MongoProperties[]): {
+  durationData: DurationProperties[],
+  histories: Map<string,HistoryProperties>,
+  photoStats2: PhotoStats2Properties,
+  timelineColors: string[],
+  timelineColors3: string[],
+  timelineData: string[][],
+  timelineData2: number[],
+  timelineData3: number[],
+  timelineLabels: string[],
+  timelineLabels3: string[],
+  timelineNameIndex: Map<string, number>,
+  timelineNameIndex3: Map<string, number>,
+} {
+
+  const histories: Map<string,HistoryProperties> = new Map();
+  const photoStats2: PhotoStats2Properties = {
+    shortestAfterReceiving: {
+      trait: '',
+      count: 0,
+      villagers: [],
+      duration: 10000,
+    },
+    longestAfterReceiving: {
+      trait: '',
+      count: 0,
+      villagers: [],
+      duration: 0,
+    },
+    longestWithoutReceiving: {
+      trait: '',
+      count: 0,
+      villagers: [],
+      duration: 0,
+    },
+  }
+
+  const timelineLabels: string[] = [];
+  const timelineData: string[][] = [];
+  const timelineColors: string[] = [];
+  const durationMap: Map<number, DurationProperties> = new Map();
+  const timelineData2: number[] = [];
+  const timelineNameIndex: Map<string, number> = new Map();
+  let i = 0;
+
+  for (const mongoDatum of mongoData) {
+    const tmpHist: HistoryProperties = {
+      ... mongoDatum
+    } as HistoryProperties;
+    tmpHist.startDateDate = new Date(mongoDatum.startDate);
+    if (!mongoDatum.endDate) {
+      tmpHist.endDateDate = new Date();
+      tmpHist.endDateDate.setHours(0, 0, 0);
+    } else {
+      tmpHist.endDateDate = new Date(mongoDatum.endDate);
+    }
+    if (mongoDatum.photo) {
+      tmpHist.photoDateDate = new Date(mongoDatum.photoDate);
+      const stayAfterReceiving = calculateDays(tmpHist.photoDateDate, tmpHist.endDateDate);
+      if (!mongoDatum.currentResident) {
+        if (stayAfterReceiving < photoStats2.shortestAfterReceiving.duration) {
+          photoStats2.shortestAfterReceiving.duration = stayAfterReceiving;
+          photoStats2.shortestAfterReceiving.villagers = [mongoDatum.name];
+        } else if (stayAfterReceiving === photoStats2.shortestAfterReceiving.duration) {
+          photoStats2.shortestAfterReceiving.villagers.push(mongoDatum.name);
+        }
+      }
+      if (stayAfterReceiving > photoStats2.longestAfterReceiving.duration) {
+        photoStats2.longestAfterReceiving.duration = stayAfterReceiving;
+        photoStats2.longestAfterReceiving.villagers = [mongoDatum.name];
+      } else if (stayAfterReceiving === photoStats2.longestAfterReceiving.duration) {
+        photoStats2.longestAfterReceiving.villagers.push(mongoDatum.name);
+      }
+    }
+    tmpHist.endDateString = tmpHist.endDateDate.toLocaleDateString("fr-CA");
+    tmpHist.duration = calculateDays(tmpHist.startDateDate, tmpHist.endDateDate);
+    if (!durationMap.has(tmpHist.duration)) {
+      durationMap.set(tmpHist.duration, {
+        trait: tmpHist.duration.toString(),
+        count: 0,
+        villagers: [],
+        duration: tmpHist.duration,
+      })
+    }
+    const tmpDuration = durationMap.get(tmpHist.duration)!;
+    tmpDuration.count++;
+    tmpDuration.villagers.push(tmpHist.name);
+    histories.set(tmpHist.name, tmpHist);
+    timelineLabels.push(mongoDatum.name);
+    timelineData.push([tmpHist.startDateString, tmpHist.endDateString])
+    timelineColors.push('#' + villagersData.get(tmpHist.name)?.title_color!)
+    timelineData2.push(tmpHist.duration);
+    timelineNameIndex.set(tmpHist.name, i);
+    i++;
+  }
+
+  const durationData = Array.from(durationMap.values());
+  durationData.sort((a, b) => b.duration - a.duration);
+
+  const timelineData3: number[] = [];
+  const timelineLabels3: string[] = [];
+  const timelineColors3: string[] = [];
+  const timelineNameIndex3: Map<string, number> = new Map();
+  i = 0;
+
+  for (const duration of durationData) {
+    for (const villager of duration.villagers) {
+      timelineLabels3.push(villager);
+      timelineData3.push(duration.duration);
+      timelineColors3.push('#' + villagersData.get(villager)?.title_color!);
+      timelineNameIndex3.set(villager, i);
+      i++;
+    }
+  }
+
+  let found = false;
+  for (const duration of durationData) {
+    for (const villager of duration.villagers) {
+      if (!(histories.get(villager)!.photo)) {
+        photoStats2.longestWithoutReceiving.duration = duration.duration;
+        photoStats2.longestWithoutReceiving.villagers.push(villager);
+        found = true;
+      }
+    }
+    if (found) {
+      break;
+    }
+  }
+
+  photoStats2.shortestAfterReceiving.trait = photoStats2.shortestAfterReceiving.duration.toString();
+  photoStats2.longestWithoutReceiving.trait = photoStats2.longestWithoutReceiving.duration.toString();
+
+  return {
+    durationData,
+    histories,
+    photoStats2,
+    timelineColors,
+    timelineColors3,
+    timelineData,
+    timelineData2,
+    timelineData3,
+    timelineLabels,
+    timelineLabels3,
+    timelineNameIndex,
+    timelineNameIndex3,
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,9 @@ import Stats from '../components/stats';
 import TopBar from '../components/topBar';
 import VillagerDialog from '../components/villagerDialog';
 import { villagersData } from '../lib/combinedData';
-import { calculateDays } from '../lib/functions';
-import { DurationProperties, HistoryProperties, MongoProperties, PhotoStats2Properties, PhotoStatsProperties, TraitProperties } from '../types';
 import getData from '../lib/getData';
+import prepareData from '../lib/prepareData';
+import { DurationProperties, HistoryProperties, MongoProperties, PhotoStats2Properties, PhotoStatsProperties, TraitProperties } from '../types';
 
 declare module '@mui/material/styles' {
   interface TypographyVariants {
@@ -127,135 +127,33 @@ export default function HomePage({ mongoData, speciesData, personalityData, gend
   const [allReady, setAllReady] = useState(false);
 
   useEffect(() => {
-    const tmpHistories: Map<string,HistoryProperties> = new Map();
-    const tmpPhotoStats2: PhotoStats2Properties = {
-      shortestAfterReceiving: {
-        trait: '',
-        count: 0,
-        villagers: [],
-        duration: 10000,
-      },
-      longestAfterReceiving: {
-        trait: '',
-        count: 0,
-        villagers: [],
-        duration: 0,
-      },
-      longestWithoutReceiving: {
-        trait: '',
-        count: 0,
-        villagers: [],
-        duration: 0,
-      },
-    }
+    const {
+      durationData,
+      histories,
+      photoStats2,
+      timelineColors,
+      timelineColors3,
+      timelineData,
+      timelineData2,
+      timelineData3,
+      timelineLabels,
+      timelineLabels3,
+      timelineNameIndex,
+      timelineNameIndex3,
+    } = prepareData(mongoData);
 
-    const labels: string[] = [];
-    const timeData: string[][] = [];
-    const backgroundColor: string[] = [];
-    const durationMap: Map<number, DurationProperties> = new Map();
-    const durationData: number[] = [];
-    const nameIndex: Map<string, number> = new Map();
-    let i = 0;
-
-    for (const mongoDatum of mongoData) {
-      const tmpHist: HistoryProperties = {
-        ... mongoDatum
-      } as HistoryProperties;
-      tmpHist.startDateDate = new Date(mongoDatum.startDate);
-      if (!mongoDatum.endDate) {
-        tmpHist.endDateDate = new Date();
-        tmpHist.endDateDate.setHours(0, 0, 0);
-      } else {
-        tmpHist.endDateDate = new Date(mongoDatum.endDate);
-      }
-      if (mongoDatum.photo) {
-        tmpHist.photoDateDate = new Date(mongoDatum.photoDate);
-        const stayAfterReceiving = calculateDays(tmpHist.photoDateDate, tmpHist.endDateDate);
-        if (!mongoDatum.currentResident) {
-          if (stayAfterReceiving < tmpPhotoStats2.shortestAfterReceiving.duration) {
-            tmpPhotoStats2.shortestAfterReceiving.duration = stayAfterReceiving;
-            tmpPhotoStats2.shortestAfterReceiving.villagers = [mongoDatum.name];
-          } else if (stayAfterReceiving === tmpPhotoStats2.shortestAfterReceiving.duration) {
-            tmpPhotoStats2.shortestAfterReceiving.villagers.push(mongoDatum.name);
-          }
-        }
-        if (stayAfterReceiving > tmpPhotoStats2.longestAfterReceiving.duration) {
-          tmpPhotoStats2.longestAfterReceiving.duration = stayAfterReceiving;
-          tmpPhotoStats2.longestAfterReceiving.villagers = [mongoDatum.name];
-        } else if (stayAfterReceiving === tmpPhotoStats2.longestAfterReceiving.duration) {
-          tmpPhotoStats2.longestAfterReceiving.villagers.push(mongoDatum.name);
-        }
-      }
-      tmpHist.endDateString = tmpHist.endDateDate.toLocaleDateString("fr-CA");
-      tmpHist.duration = calculateDays(tmpHist.startDateDate, tmpHist.endDateDate);
-      if (!durationMap.has(tmpHist.duration)) {
-        durationMap.set(tmpHist.duration, {
-          trait: tmpHist.duration.toString(),
-          count: 0,
-          villagers: [],
-          duration: tmpHist.duration,
-        })
-      }
-      const tmpDuration = durationMap.get(tmpHist.duration)!;
-      tmpDuration.count++;
-      tmpDuration.villagers.push(tmpHist.name);
-      tmpHistories.set(tmpHist.name, tmpHist);
-      labels.push(mongoDatum.name);
-      timeData.push([tmpHist.startDateString, tmpHist.endDateString])
-      backgroundColor.push('#' + villagersData.get(tmpHist.name)?.title_color!)
-      durationData.push(tmpHist.duration);
-      nameIndex.set(tmpHist.name, i);
-      i++;
-    }
-
-    const tmpDurations = Array.from(durationMap.values());
-    tmpDurations.sort((a, b) => b.duration - a.duration);
-
-    const durationData2: number[] = [];
-    const labels2: string[] = [];
-    const colors2: string[] = [];
-    const nameIndex2: Map<string, number> = new Map();
-    i = 0;
-
-    for (const duration of tmpDurations) {
-      for (const villager of duration.villagers) {
-        labels2.push(villager);
-        durationData2.push(duration.duration);
-        colors2.push('#' + villagersData.get(villager)?.title_color!);
-        nameIndex2.set(villager, i);
-        i++;
-      }
-    }
-
-    let found = false;
-    for (const duration of tmpDurations) {
-      for (const villager of duration.villagers) {
-        if (!(tmpHistories.get(villager)!.photo)) {
-          tmpPhotoStats2.longestWithoutReceiving.duration = duration.duration;
-          tmpPhotoStats2.longestWithoutReceiving.villagers.push(villager);
-          found = true;
-        }
-      }
-      if (found) {
-        break;
-      }
-    }
-
-    tmpPhotoStats2.shortestAfterReceiving.trait = tmpPhotoStats2.shortestAfterReceiving.duration.toString();
-    tmpPhotoStats2.longestWithoutReceiving.trait = tmpPhotoStats2.longestWithoutReceiving.duration.toString();
-
-    setHistories(tmpHistories);
-    setTimelineData(timeData);
-    setTimelineData2(durationData);
-    setTimelineLabels(labels);
-    setTimelineColors(backgroundColor);
-    setDurationData(tmpDurations);
-    setTimelineData3(durationData2);
-    setTimelineLabels3(labels2);
-    setTimelineColors3(colors2);
-    setTimelineNameIndex(nameIndex);
-    setTimelineNameIndex3(nameIndex2);
-    setPhotoStats2(tmpPhotoStats2);
+    setHistories(histories);
+    setTimelineData(timelineData);
+    setTimelineData2(timelineData2);
+    setTimelineLabels(timelineLabels);
+    setTimelineColors(timelineColors);
+    setDurationData(durationData);
+    setTimelineData3(timelineData3);
+    setTimelineLabels3(timelineLabels3);
+    setTimelineColors3(timelineColors3);
+    setTimelineNameIndex(timelineNameIndex);
+    setTimelineNameIndex3(timelineNameIndex3);
+    setPhotoStats2(photoStats2);
   }, [mongoData])
 
   useEffect(() => {
