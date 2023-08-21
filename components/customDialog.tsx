62976d16cd680e1fe7a990d34458a77d2a69fd4a
arkteck <arkteck@gmail.com>
2023-08-21 12:00:29 -0400

    set dialog maxWidth of xl

--- components/customDialog.tsx
+++ components/customDialog.tsx
@@ -12,6 +12,7 @@ export default function CustomDialog({zIndex, ...props}: CustomDialogProps) {
   return <>
     <Dialog
       {...props}
+      maxWidth="xl"
       sx={{
         zIndex,
       }}
