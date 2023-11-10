36e200bcd4ceef2a3afdbb6a731cdf5f0c0be6ff
arkteck <arkteck@gmail.com>
2023-11-10 13:44:04 -0500

    change elevation on hover for Cards

--- components/card.tsx
+++ components/card.tsx
@@ -2,16 +2,18 @@ import AccessTimeRoundedIcon from '@mui/icons-material/AccessTimeRounded';
 import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
 import ArrowForwardRoundedIcon from '@mui/icons-material/ArrowForwardRounded';
 import CameraAltRoundedIcon from '@mui/icons-material/CameraAltRounded';
-import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded';
-import { Box, IconButton, Paper, Stack, Typography, useTheme } from '@mui/material';
+import { Box, Paper, Stack, Typography, useTheme } from '@mui/material';
 import Image from 'next/image';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { dayOrDays } from '../lib/functions';
+import rgbDataURL from '../lib/rgbDataURL';
 import { DataContext, ScreenContext } from '../pages';
 import { HistoryProperties, VillagerProperties2 } from '../types';
 import CRIcon from './crIcon';
 import IconWithText from './iconWithText';
-import rgbDataURL from '../lib/rgbDataURL';
+
+const lowElevation = 4;
+const highElevation = 14;
 
 export default function HistoryCard({ history, villagerData }: {
   history: HistoryProperties,
@@ -24,10 +26,11 @@ export default function HistoryCard({ history, villagerData }: {
   } = useContext(DataContext);
   const { mediumScreen } = useContext(ScreenContext);
   const theme = useTheme();
+  const [elevation, setElevation] = useState(lowElevation);
 
   return (
     <Paper
-      elevation={5}
+      elevation={elevation}
       sx={{
         background: theme.palette.success.light,
         position: 'relative',
@@ -36,6 +39,12 @@ export default function HistoryCard({ history, villagerData }: {
       onClick={() => {
         setDialogVillager(history.name);
         setShowVillagerDialog(true);
+      }}
+      onMouseOver={() => {
+        setElevation(highElevation);
+      }}
+      onMouseOut={() => {
+        setElevation(lowElevation);
       }}
     >
       <Stack direction="row">
